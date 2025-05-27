dd94bb628f69d5f58407d9dc1ef3b9e93079481b
vidosits <vidosits>
2025-05-27 14:29:04 +0200

    Fix Host type json field

--- types.go
+++ types.go
@@ -1,30 +1,5 @@
 package recommendation
 
-type AuthenticationType string
-
-const (
-	InternalAuthentication AuthenticationType = "internal"
-	BasicAuthentication    AuthenticationType = "basic"
-)
-
-type BasicAuth struct {
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
-	Hash     string `json:"hash,omitempty"`
-}
-
-type Authentication struct {
-	Type      AuthenticationType `json:"type"`
-	BasicAuth BasicAuth          `json:"basicAuth,omitempty"`
-}
-
-type URL struct {
-	Authentication Authentication `json:"authentication"`
-	Subdomain      string         `json:"subdomain"`
-	Domain         string         `json:"domain"`
-	Port           int64          `json:"port"`
-}
-
 type ResourceRequest struct {
 	Memory int64 `json:"memory"`
 	CPU    int64 `json:"cpu"`
@@ -41,43 +16,8 @@ type Resources struct {
 	ShmSize  int64           `json:"shmSize"`
 }
 
-// PortBinding represents a binding between a Host IP address and a Host Port
-type PortBinding struct {
-	// HostIP is the host IP Address
-	HostIP string `json:"HostIp"`
-	// HostPort is the host port number
-	HostPort string
-}
-
-// PortMap is a collection of PortBinding indexed by Port
-type PortMap map[Port][]PortBinding
-
-// PortSet is a collection of structs indexed by Port
-type PortSet map[Port]struct{}
-
-// Port is a string containing port number and protocol in the format "80/tcp"
-type Port string
-
-type StartupConfiguration struct {
-	Command   []string `json:"command,omitempty"`
-	Arguments []string `json:"arguments,omitempty"`
-}
-
-type VersionSpecifier string
-
-const (
-	ExactVersion         VersionSpecifier = "exactly"
-	MinimumVersion       VersionSpecifier = "minimum"
-	MaximumVersion       VersionSpecifier = "maximum"
-	ExcludeVersion       VersionSpecifier = "exclude"
-	CompatibleVersion    VersionSpecifier = "compatible"
-	UnconstrainedVersion VersionSpecifier = "unconstrained"
-	CustomVersion        VersionSpecifier = "custom"
-)
-
 type Version struct {
-	Specifier  VersionSpecifier `json:"specifier,omitempty"`
-	Constraint string           `json:"constraint"`
+	Constraint string `json:"constraint"`
 }
 
 type Package struct {
@@ -101,7 +41,7 @@ type Image struct {
 }
 
 type Host struct {
-	TotalMemory int64 `json:"total_memory"`
+	TotalMemory int64 `json:"totalMemory"`
 }
 
 type Workspace struct {
